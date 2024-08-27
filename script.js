5f9bc8c36e681183359602002815ec327a626ec3
Matthew Richardson <matthew_richardson@ymail.com>
2024-08-27 13:46:39 +0100

    tested lightSwitch = !lightSwitch; it toggles and works

--- script.js
+++ script.js
@@ -13,33 +13,21 @@ let fan = false;
 let waterValve = false;
 let heater = false;
 
+function lightFunction() {
+  lightSwitch = !lightSwitch;
 
-function lightFunction(){ //For now to make the light button work without maths
-    //on click eventlistener needed for button
-    //refer to previous projects to make easier.
-    if (lightSwitch = true){
-        lightPower++
-    }
-
-    //need to use a loop when light function = true.
-    //need toggle on and off for lightSwitch
-
-
-    //get HTML lightSwitch ID div and add click ability with toggle on and off activator function
+  /* //get HTML lightSwitch ID div and add click ability with toggle on and off activator function
     document.getElementById("lightSwitch").addEventListener("click"), function lightToggle() {
         lightSwitch = !lightSwitch;
     }
+*/
+document.getElementById("lightButton").addEventListener("click", function lightIncrease() { 
+    lightPower+= 1;
 }
 
+}
 
+lightFunction();
+lightFunction();
 
-
-
-
-function windowFunction(){}
-
-function fanFunction(){}
-
-function waterFunction(){}
-
-function heaterFunction(){}
+console.log(lightSwitch);
