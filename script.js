39aafd32073b71001a89b393acf3a5fd4919978c
Matthew Richardson <matthew_richardson@ymail.com>
2024-08-27 10:58:06 +0100

    updated variables and tidied a little

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//Sensors
 let plantGrowth = 0;
-let tempPercentage = 0;
+let temperature = 0;
 let humidity = 0;
 let soilWetness = 0;
 let lightPower = 0;
-let window = true;
-let fan = false;
 let time = 1000;
 
+//Activators/Adjustables
+let lightSwitch = false;
+let window = true;
+let fan = false;
+let waterValve = false;
+let heater = false;
