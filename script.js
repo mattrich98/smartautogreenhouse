23b42689eb38a54b85ba7e0202b9d9fbb89c19e2
Matthew Richardson <matthew_richardson@ymail.com>
2024-08-27 13:32:36 +0100

    attempt to make lightswitch toggle boolean, need console log to see

--- script.js
+++ script.js
@@ -17,11 +17,18 @@ let heater = false;
 function lightFunction(){ //For now to make the light button work without maths
     //on click eventlistener needed for button
     //refer to previous projects to make easier.
-    lightPower++;
-    temperature++;
+    if (lightSwitch = true){
+        lightPower++
+    }
 
     //need to use a loop when light function = true.
     //need toggle on and off for lightSwitch
+
+
+    //get HTML lightSwitch ID div and add click ability with toggle on and off activator function
+    document.getElementById("lightSwitch").addEventListener("click"), function lightToggle() {
+        lightSwitch = !lightSwitch;
+    }
 }
 
 
