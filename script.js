9daceaafa881c338996912402c3ef701cfff9148
Matthew Richardson <matthew_richardson@ymail.com>
2024-09-02 06:40:49 +0100

    for loop cleaned up

--- script.js
+++ script.js
@@ -14,64 +14,22 @@ let waterValve = false;
 let heater = false;
 
 
-//Light Function Testing ----------------------------------------
-
 
 function lightFunction() {
   lightSwitch = !lightSwitch;
 
-  /*document.getElementById("lightButton").addEventListener("click", function lightIncrease() { 
-    lightPower+= 1;
-}*/
-
-buttonIncrementer = i;
 if(lightSwitch == true){
-
-  if(lightSwitch == true){
     console.log("test");
     for (let i = 0; i <= 100; i++) { 
     console.log(i);
   }
 }
 
-for (let i = 0; i >= 100; i--) {
-  //console.log(i);
+if(lightSwitch == false){
+    console.log("test");
+    for (let i = 0; i >= 100; i--) { 
+    console.log(i);
+  }
 }
 
-console.log(buttonIncrementer);
-
-/*while(lightSwitch == true && (lightPower != 100)){
-    //console.log('testing this works');
-    lightPower++
-    console.log(lightPower);
-    //lightPower <= 100 then it will increment upwards
 }
-
-while(lightSwitch == false && (lightPower != 0)){
-    lightPower--
-    console.log(lightPower);
-
-}
-/*
-  } else {
-    console.log('opposite');
-    //lightPower > 0 then it will increment downwards
-
-    lightPower--
-}
-*/ 
-}
-
-lightFunction();
-
-lightFunction();
-
-
-console.log(lightPower);
-
-//Light Function Testing ----------------------------------------
-
-/*if (lightSwitch == true && (lightPower <= 100)) {
-    console.log('testing this works');
-    lightPower++
-    //lightPower <= 100 then it will increment upwards*/
